1f9aad1b1ee8b70a65bf0242abbaab0f3995d605
Namita Bhatawdekar <nbhatawdekar@mediaocean.com>
2016-09-13 21:51:55 +0530

    fixed issue with creating hierarchy

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ d3.csv("./data/nagpur_budget_data.csv", function(csv_data) {
 
 
     function getNestedData(n) {
-        //n = 3;
         var sub_levels = levels.slice(0,n);
         var nested_data = d3.nest()
 
@@ -23,61 +22,35 @@ d3.csv("./data/nagpur_budget_data.csv", function(csv_data) {
                     })
                 }
             })
-            .entries(csv_data);
+            .map(csv_data);
 
         return nested_data;
 
     }
 
+    function findInMap(data, name) {
+        var subData = undefined;
+        for (key in data){
+            if (key === name) {
+                return data[key];
+            }
 
-    function find(theObject, parent) {
-        var result = null;
-        if(theObject instanceof Array) {
-            for(var i = 0; i < theObject.length; i++) {
-                result = find(theObject[i], parent);
-                if (result) {
-                    break;
-                }
+        }
+        for (key in data) {
+            subData = findInMap(data[key], name)
+            if (subData) {
+                return subData;
             }
         }
-        else
-        {
-            for(var prop in theObject) {
-                console.log(prop + ': ' + theObject[prop]);
-                if(prop == 'key') {
-                    if(theObject[prop] == parent) {
-                        return theObject;
-                    }
-                }
-                if(theObject[prop] instanceof Object || theObject[prop] instanceof Array) {
-                    result = find(theObject[prop], parent);
-                    if (result) {
-                        break;
-                    }
-                }
-            }
-        }
-        return result;
+
     }
-
     function getDataAtLevel(parent, level, data, parentHierarchy) {
         // search for parent root in nested_data and return children
-
         if (parent !== null) {
-            //parentData = _.findKey(csv_data, level);
-            //var dataObj = _.find(data, function(obj) {
-            //    return obj.key === parent;
-            //
-            //});
-            //var dataObj = _.findDeep(data, { 'key': ''+ parent + '' })
             if (parentHierarchy) {
-                data = _.find(data, function(obj) {
-                    return obj.key === parentHierarchy;
-                });
+                data = data[parentHierarchy];
             }
-
-            dataObj = find(data, parent);
-            subData = (dataObj !== null && dataObj.values !== undefined) ? dataObj.values : dataObj;
+            var subData = findInMap(data, parent);
 
         }
         return subData;
@@ -85,20 +58,16 @@ d3.csv("./data/nagpur_budget_data.csv", function(csv_data) {
     }
 
 
-    function toggleActiveCSS() {
-
-
-    }
-
     function displayLevel(parent, level, data) {
-        // if level textarea already present, replace data; else create textarea
         var nested_data = getNestedData(1),
             subData;
-        //var first_level_data = getDataAtLevel(null,nested_data);
-        //var text;
         if (parent !== null) {
-            var parentHierarchy = (activeHierarchy[1] !== undefined) ? activeHierarchy[1].attr("value") : null;
-            data = getDataAtLevel(parent, level, data)
+            var parentHierarchy;
+            if (level !== 2) {
+                parentHierarchy = (activeHierarchy[1] !== undefined) ? activeHierarchy[1].attr("value") : null;
+            }
+
+            data = getDataAtLevel(parent, level, data, parentHierarchy)
         }
         data = (data !== undefined) ? data : nested_data;
         var div = d3.select(".container")
@@ -108,77 +77,57 @@ d3.csv("./data/nagpur_budget_data.csv", function(csv_data) {
             var existing_div = $('.' + selector);
             existing_div.html("");
             div = d3.select('.' + selector);
-
             // if selector div already exists, remove children of that div
             for (var k = level-1; k < levels.length; k++) {
                 var child_div_selector = levels[k].replace(" ","") + '_parent';
                 var child_div = $('.' + child_div_selector);
                 child_div.html("");
             }
-
         }
         else {
             div = div.append('div')
                 .attr("class", "box " + selector)
         }
 
-
-
-               div.selectAll('div')
-                .data(data)
-                .enter()
-                .append("div")
-                //.append("span")
-                .attr("class", levels[level-1])
-                   .attr("value", function(d){
-                       return d.key;
-                   })
-                .html(function(d, i){
-                       if ((level) >= (levels.length) ) {
-                           return '<i class="fa fa-file" aria-hidden="true"></i> <span class="key">' + d.key  + '</span>' + ((d.value !== undefined) ? " : <span class='num'>" + d.value.total_spend.toFixed(2) : '</span>');
-                       } else {
-                           return '<i class="fa fa-folder" aria-hidden="true"></i> <span class="key">' + d.key  + '</span>' + ((d.value !== undefined) ? " : <span class='num'>" + d.value.total_spend.toFixed(2) + '</span><i class="fa fa-caret-right" aria-hidden="true"></i>' : "");
+       div.selectAll('div')
+        .data(d3.entries(data))
+        .enter()
+        .append("div")
+        .attr("class", levels[level-1])
+           .attr("value", function(d){
+               return d.key;
+           })
+        .html(function(d, i){
+           if (d.value.total_spend !== undefined) {
+               if ((level) >= (levels.length) ) {
+                   return '<i class="fa fa-file" aria-hidden="true"></i> <span class="key">' + d.key  + '</span>' + ((d.value !== undefined) ? " : <span class='num'>" + d.value.total_spend.toFixed(2) : '</span>');
+               } else {
+                   return '<i class="fa fa-folder" aria-hidden="true"></i> <span class="key">' + d.key  + '</span>' + ((d.value !== undefined) ? " : <span class='num'>" + d.value.total_spend.toFixed(2) + '</span><i class="fa fa-caret-right" aria-hidden="true"></i>' : "");
+               }
+           }
+        })
+        .on("click", function(d, e, f) {
+            var className = f[e].className.replace(" active","");
+            var click_level =  levels.indexOf(className) + 1;
+            subData = getNestedData(click_level+1);
+            var parent = (d !== undefined && d !== null && d.key) ? d.key : (d !== undefined && d !== null) ? d : null;
+            if (click_level < levels.length) {
+               displayLevel(parent, click_level+1,  subData)
+               var keys = Object.keys(activeHierarchy);
+               if (keys.indexOf(click_level.toString()) >= 0) {
+                   for (var j = click_level; j <= levels.length; j++) {
+                       if (activeHierarchy[j] !== undefined) {
+                           activeHierarchy[j].classed("active", false);
+                           delete activeHierarchy[j];
                        }
+                   }
+               }
+               d3.select(this).classed("active", true);
+               activeHierarchy[click_level] = d3.select(this);
+           }
 
-                })
-                .on("click", function(d, e, f) {
-                       var className = f[e].className.replace(" active","");
-                    var click_level =  levels.indexOf(className) + 1;
-                    subData = getNestedData(click_level+1);
-                    var parent = (d !== undefined && d !== null && d.key) ? d.key : (d !== undefined && d !== null) ? d : null;
-                    displayLevel(parent, click_level+1,  subData)
-                       //$('.active').removeCSS('active');
-
-
-
-                       var keys = Object.keys(activeHierarchy);
-                       if (keys.indexOf(click_level.toString()) >= 0) {
-                           for (var j = click_level; j <= levels.length; j++) {
-                               if (activeHierarchy[j] !== undefined) {
-                                   activeHierarchy[j].classed("active", false);
-                                   delete activeHierarchy[j];
-                               }
-
-
-                           }
-                       }
-
-                       d3.select(this).classed("active", true);
-                       activeHierarchy[click_level] = d3.select(this);
-
-
-
-
-                });
-        }
-
-        //_.forEach(nested_data, function(value, key) {
-        //    div.append($('<div class='+ levels[level] + '>' + value.key + " : " + value.value.total_spend + '</div>' + "\n") );
-        //
-        //});
-        //div.add(text);
-
-
+        });
+    }
 
     displayLevel(null,1);
 
@@ -187,6 +136,4 @@ d3.csv("./data/nagpur_budget_data.csv", function(csv_data) {
         displayLevel(null,1);
 
     });
-
-    //d3.select(".container").selectAll('textarea').html(keylist.keys());
 });
